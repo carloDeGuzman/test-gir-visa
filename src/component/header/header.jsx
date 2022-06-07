617120a778a6c60ca2db61655152dcca12383619
carloDeGuzman <35952904+carloDeGuzman@users.noreply.github.com>
2022-06-07 14:59:38 +0800

    Update header.jsx

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
           <Row>
             <Col>
               <div>
-                <h1 className="headerTitle">PRO VISA RUN</h1>
+                <h1 className="headerTitle">TOP VISA RUN</h1>
               </div>
             </Col>
           </Row>
